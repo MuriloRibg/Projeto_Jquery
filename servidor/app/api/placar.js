ab8eecd75a74c434d34b761b241fec73beba36b0
Murilo Ribeiro <murilos.ribeiro21@gmail.com>
2021-12-29 22:28:56 -0300

    Adicionando os arquivos

---
+++ servidor/app/api/placar.js
@@ -0,0 +1,25 @@
+const db = require('../../config/database')
+
+const api = {};
+
+
+api.lista = function(req, res) {
+
+    db.find({}).exec(function(err, doc) {
+      if (err) return console.log(err);
+      res.json(doc);
+  });
+};
+
+api.insere = function(req, res){
+    db.remove({}, { multi: true }, function (err, numRemoved) {
+    });
+
+    db.insert(req.body.placar, function(err, newDoc) {
+        if(err) return console.log(err);
+        console.log('Sincronizado com sucesso: ' + newDoc._id);
+        res.json({});
+    });
+}
+
+module.exports = api;
